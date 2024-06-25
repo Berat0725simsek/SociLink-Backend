60e86c85b527c844a674329f8a804b72f0b43d26
Berat0725simsek <110166453+Berat0725simsek@users.noreply.github.com>
2024-06-25 13:33:01 +0300

    uptade index.js

--- index.js
+++ index.js
@@ -1,20 +1,38 @@
-const express = require("express")
-
-require("dotenv").config()
-
-
+const express = require('express');
 const app = express()
 
-const PORT = 4000 || process.env.PORT
+const mongoose = require('mongoose');
+
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const userRouter = require("./routers/userRouter");
+
+require('dotenv').config()
+
+const PORT = 3000 || process.env.PORT
+
+const mongoURI = process.env.MONGO_URI; // Veritabanı adını burada değiştirin
+
+app.use(cors())
+app.use(bodyParser.json())
+
+mongoose.connect(mongoURI)
+.then(() => {
+  console.log('MongoDB bağlantısı başarılı.');
+})
+.catch((err) => {
+  console.error('MongoDB bağlantı hatası:', err);
+});
+
+
+app.use('/user', userRouter);
 
 app.get("/",(req,res)=>{
     res.send("merhaba")
 })
 
 
-app.get("/car",(req,res)=>{
-    res.send("car merhaba")
-})
-
-
 app.listen(PORT,() => console.log(`server ${PORT} nunda calisdi`))
+
+module.exports = app
